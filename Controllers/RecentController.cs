a4e574b4dec08ca5568410d7bd5554df2bba8284
anhquan7826 <anhquan7826@gmail.com>
2022-11-28 20:46:42 +0700

    feat: add recent song

---
+++ Controllers/RecentController.cs
@@ -0,0 +1,37 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tourmaline.Models;
+using tourmaline.Services;
+
+namespace tourmaline.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RecentController : ControllerBase
+{
+    public RecentController(DbConnection dbConnection)
+    {
+        _database = dbConnection;
+    }
+
+    private DbConnection _database;
+
+    [Route("api/[controller]/recents")]
+    [Authorize]
+    public async Task<ActionResult<Recents>> GetRecentSongs()
+    {
+        var username = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value!;
+        var songIDs = await _database.Read("recents", new Dictionary<string, dynamic>() { { "username", username } },
+            new List<string>() { "song" }, "added_date");
+        var recents = new Recents();
+        var songController = new SongController(_database);
+        foreach (int song in songIDs.First()["song"])
+        {
+            var result = (await songController.GetSongInfo(song)).Value!;
+            recents.RecentSongs.Add(result);
+        }
+
+        return Ok(recents);
+    }
+}
---
+++ Models/Recents.cs
@@ -0,0 +1,11 @@
+namespace tourmaline.Models;
+
+public class Recents
+{
+    public Recents(List<Song>? recentSongs = null)
+    {
+        RecentSongs = recentSongs ?? new List<Song>();
+    }
+    
+    public List<Song> RecentSongs;
+}
