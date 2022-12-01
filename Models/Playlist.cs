bcd326b916797faaf876f8f9ac785a877fb053af
anhquan7826 <anhquan7826@gmail.com>
2022-12-01 11:32:56 +0700

    update login, signup

--- Models/Playlist.cs
+++ Models/Playlist.cs
@@ -2,18 +2,16 @@ namespace tourmaline.Models;
 
 public class Playlist
 {
-    public Playlist(int id = -1, string username = "", string name = "", string cover = "")
+    public Playlist(int id = -1, string username = "", string name = "")
     {
         Id = id;
         Name = name;
         UserName = username;
         Songs = new List<Song>();
-        Cover = cover;
     }
 
     public int Id { get; set; }
     public string UserName { get; set; }
     public string Name { get; set; }
-    public string Cover { get; set; }
     public List<Song> Songs { get; set; }
 }
