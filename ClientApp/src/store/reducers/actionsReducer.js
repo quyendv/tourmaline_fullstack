aeb15022822d424fed3c97ecef5cd7d71a5d1d4d
KaoSon2004 <gsu0987q@gmail.com>
2022-12-05 20:26:20 +0700

    handle creplaylist not rerender

--- ClientApp/src/store/reducers/actionsReducer.js
+++ ClientApp/src/store/reducers/actionsReducer.js
@@ -1,21 +1,30 @@
-const {actionTypes} = require("../actions/actionTypes")
+import { actionTypes } from "../actions/actionTypes";
 
 const initState = {
     setIsOpenModal: function () {
-        return 0
-    }
-}
+        return 0;
+    },
+    createPlaylist: function () {
+        return 0;
+    },
+};
 
 const actionsReducer = (state = initState, action) => {
     switch (action.type) {
         case actionTypes.SET_IS_OPEN_MODAL:
             return {
                 ...state,
-                setIsOpenModal: action.setIsOpenModal
-            }
-        default:
-            return state
-    }
-}
+                setIsOpenModal: action.setIsOpenModal,
+            };
 
-export default actionsReducer
+        case actionTypes.CREATE_PLAYLIST:
+            return {
+                ...state,
+                createPlaylist: action.createPlaylist,
+            };
+        default:
+            return state;
+    }
+};
+
+export default actionsReducer;
