937dee1bff1e005969240bc31f744e03fb68f66e
KaoSon2004 <gsu0987q@gmail.com>
2022-12-13 13:38:54 +0700

    raw comment modal

--- ClientApp/src/components/CommentModal/CommentModal.js
+++ ClientApp/src/components/CommentModal/CommentModal.js
@@ -1,29 +1,53 @@
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { icons } from '../../utils/icons';
-import * as apis from '../../services/comment'
-
-
+import * as apis from '../../services/comment';
+import moment from 'moment';
 
 const { AiOutlineClose, FaRegComment } = icons;
 
 function CommentModal() {
     const { setIsOpenCommentModal } = useSelector((state) => state.actions);
     const [commentValue, setCommentValue] = useState('');
-    const {token} = useSelector(state => state.auth)
-    const {commentSongId} = useSelector(state => state.music)
+    const { token } = useSelector((state) => state.auth);
+    const { commentSongId } = useSelector((state) => state.music);
+    const [comments, setComments] = useState([]);
 
-    const handlePostComment = (e) => {
+    useEffect(() => {
+        const fetchComment = async () => {
+            const response = await apis.getAllComment(commentSongId, token);
+            if (response.status == 200) {
+                console.log(response);
+                setComments(response.data);
+            }
+        };
+        fetchComment();
+    }, []);
+    const handlePostComment = async (e) => {
         const finalPayload = {
             id: commentSongId,
-            content: commentValue
+            content: commentValue,
+        };
+        if (e.keyCode == '13') {
+            const response = await apis.postComment(finalPayload, token);
+            setCommentValue('');
+
+            console.log(response);
         }
-        if(e.keyCode == '13') {
-            const response = apis.postComment(finalPayload, token)
-            console.log(response)
-        }
+    };
+
+    const handleEditComment = async () => {
+        const response = await apis.editComment(commentSongId, token)
+        console.log(response)
     }
+
+    const handleDeleteComment = async () => {
+        const response = await apis.deleteComment(commentSongId, token)
+        console.log(response)
+        
+    }
+
     return (
         <div className="h-full w-full">
             <div className="fixed top-0 right-0 bottom-0 left-0 z-40 bg-[#000] opacity-30"></div>
@@ -37,21 +61,35 @@ function CommentModal() {
                         <AiOutlineClose size={20} />
                     </span>
                 </div>
-                <div className='px-3'>
-                    <span className='text-[#000] opacity-60'>0 bình luận</span>
-                    <div className='w-full h-full flex flex-col  items-center justify-center flex-auto'>
-                        <span className='opacity-30'><FaRegComment size={50}/></span>
-                        <h3 className='opacity-60'>
-                            Chưa có bình luận nào
-                        </h3>
-                    </div>
+                <div className="relative w-full px-3">
+                    <span className="text-[#000] opacity-60">{`${comments.length} bình luận`}</span>
+                    {comments.length == 0 ? (
+                        <div className="flex h-full w-full flex-auto  flex-col items-center justify-center">
+                            <span className="opacity-30">
+                                <FaRegComment size={50} />
+                            </span>
+                            <h3 className="opacity-60">Chưa có bình luận nào</h3>
+                        </div>
+                    ) : (
+                        <div>
+                            {comments.map((item, index) => (
+                                <div key={index}>
+                                    <span>{`${item.username}: `}</span>
+                                    <span className="overflow-hidden">{item.content} </span>
+
+                                    <span className="text-xs opacity-30">{moment(item.createTime).fromNow()}</span>
+                                </div>
+                            ))}
+                        </div>
+                    )}
                     <input
-                        className="mt-2 w-full rounded-l-full rounded-r-full bg-[#000]/5 px-4 py-1 text-xl"
+                        className="fixed bottom-2 right-1 left-1 mt-2 h-[40px] rounded-l-full rounded-r-full bg-[#000]/5 px-4 py-1 text-xl"
                         placeholder="Write a comment"
                         value={commentValue}
                         onChange={(e) => setCommentValue(e.target.value)}
                         onKeyUp={handlePostComment}
                     />
+                    <div className="h-[50px]"></div>
                 </div>
             </div>
         </div>
