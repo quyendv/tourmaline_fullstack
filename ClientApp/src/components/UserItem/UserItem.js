fa6fd045e9794c337d1e41596135c2332bc47db0
quyendv <daovanquyen45@gmail.com>
2022-12-18 12:58:17 +0700

    UserItem

--- ClientApp/src/components/UserItem/UserItem.js
+++ ClientApp/src/components/UserItem/UserItem.js
@@ -2,23 +2,25 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import * as apis from '../../services';
-function UserItem({ userData }) {
+import { AiOutlineUserAdd } from 'react-icons/ai';
+
+function UserItem({ userData, className }) {
     const [userAvatar, setUserAvatar] = useState('');
     const [isFollow, setIsFollow] = useState(false);
     const navigate = useNavigate();
-    const {token} = useSelector(state => state.auth)
+    const { token } = useSelector((state) => state.auth);
     const handleFollow = () => {
-        setIsFollow(prev => !prev)
-        const follow = async() => {
-            const response = await apis.follow(userData.username, token)
-            console.log(response)
-        }
-        const Unfollow = async()=> {
-            const response = await apis.unFollow(userData.username, token)
-            console.log(response)
-        }
-        follow()
-    }
+        setIsFollow((prev) => !prev);
+        const follow = async () => {
+            const response = await apis.follow(userData.username, token);
+            console.log(response);
+        };
+        const Unfollow = async () => {
+            const response = await apis.unFollow(userData.username, token);
+            console.log(response);
+        };
+        follow();
+    };
     useEffect(() => {
         const url = '';
         const fetchUserAvatar = async () => {
@@ -34,16 +36,30 @@ function UserItem({ userData }) {
     }, []);
 
     return (
-        <div className="flex w-1/5 flex-col">
-            <img
-                className="cursor-pointer"
-                onClick={() => navigate(`/user/${userData.username}`)}
-                src={userAvatar}
-            ></img>
-            <span>{userData.name}</span>
-            <span onClick={handleFollow} className='border border-white'>
+        <div className="flex w-full flex-col items-center gap-3">
+            {/* Img */}
+            <div className="h-0 w-full pb-[100%]">
+                <img
+                    className="h-auto w-full cursor-pointer rounded-full object-cover"
+                    onClick={() => navigate(`/user/${userData.username}`)}
+                    // src={userAvatar}
+                    src="https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/avatars/8/a/a/b/8aab7a0386dd9c24b90adcc5ef5a7814.jpg"
+                    alt="avatar"
+                />
+            </div>
+            {/* Content */}
+            <div className="flex flex-col items-center">
+                <span>{userData.name}</span>
+                <span className="text-xs text-[#ffffff80]">2.4M followers</span> {/* hardcode */}
+            </div>
+            {/* Follow btn */}
+            <div
+                onClick={handleFollow}
+                className="flex items-center gap-1 rounded-full bg-[var(--follow-btn-bg)] px-6 py-1.5"
+            >
+                <AiOutlineUserAdd />
                 <button>{isFollow ? 'Follow' : 'Unfollow'}</button>
-            </span >
+            </div>
         </div>
     );
 }
