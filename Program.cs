5b46271308b231b426fa2138a912a13caa2965fd
quyendv <daovanquyen45@gmail.com>
2022-12-20 00:47:50 +0700

    fix program.cs

--- Program.cs
+++ Program.cs
@@ -28,16 +28,10 @@ var connHostNameEnv = Environment.GetEnvironmentVariable("MYSQL_SERVICE_HOST");
 var connHostPortEnv = Environment.GetEnvironmentVariable("MYSQL_SERVICE_PORT");
 
 var connString = $"Server={connHostNameEnv ?? "localhost"};" +
-<<<<<<< Updated upstream
-                 $"User ID=root;Password=123321asdson;" +
+                 $"User ID=root;Password=password;" +
                  $"Port={connHostPortEnv ?? "3306"};" +
-                 $"Database=tourmaline;";
-                 
-=======
-                 $"User ID=root;Password=Nalquin4599!;" +
-                 $"Port={connHostPortEnv ?? "3306"};" +
-                 $"Database=tourmaline;";
->>>>>>> Stashed changes
+                 $"Database=tourmaline;" +
+                 $"TlsCipherSuites=TLS_DHE_RSA_WITH_AES_256_GCM_SHA384";
 
 var database = new Database(connString);
 services.AddSingleton(_ => database);
